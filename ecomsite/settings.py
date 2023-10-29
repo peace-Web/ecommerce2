a40cce9b7ac81fa58c9ff2df385a2b1c959ac661
ayane <mohssineayane35@gmail.com>
2023-10-29 21:12:19 +0100

    custom static

--- ecomsite/settings.py
+++ ecomsite/settings.py
@@ -33,6 +33,7 @@ CSRF_TRUSTED_ORIGINS = ['https://ecommerce2-production-8d31.up.railway.app','htt
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
